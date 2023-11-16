bb1f485f12beb666b8fe7f02984a68fc0abbffac
Quadrollopo <davide97@gmail.com>
2023-11-16 11:20:59 +0100

    Updated tables

--- backend/DAO.js
+++ backend/DAO.js
@@ -1,19 +1,25 @@
 import sqlite3 from 'sqlite3'
 
 const db = new sqlite3.Database('grillo.db', (err) => { if (err) throw err; });
-db.run('CREATE TABLE IF NOT EXISTS users (id STRING PRIMARY KEY, minutes INTEGER, inlab BOOLEAN, lastUpdate TIMESTAMP, lastMinutes INTEGER, HasKey BOOLEAN)');
-db.run('CREATE TABLE IF NOT EXISTS audit (user_id STRING PRIMARY KEY, title TEXT,  content TEXT)');
-db.run('CREATE TABLE IF NOT EXISTS booking (user_id STRING PRIMARY KEY, time TIMESTAMP SECONDARY KEY)');
+// USER table
+db.run('CREATE TABLE IF NOT EXISTS user (id STRING PRIMARY KEY, minutes INTEGER, inlab BOOLEAN, lastUpdate TIMESTAMP, lastMinutes INTEGER, hasKey BOOLEAN)');
+// AUDIT table
+db.run('CREATE TABLE IF NOT EXISTS audit (user_id STRING, time DATETIME, enter BOOLEAN, motivation TEXT, PRIMARY KEY (user_id, time), FOREIGN KEY(user_id) REFERENCES user(id))');
+//BOOKING table
+db.run('CREATE TABLE IF NOT EXISTS booking (user_id STRING, time DATETIME, PRIMARY KEY (user_id, time), FOREIGN KEY(user_id) REFERENCES user(id))');
+//CIT table
+db.run('CREATE TABLE IF NOT EXISTS cit (user_id STRING, phrase TEXT, id INTEGER PRIMARY KEY, FOREIGN KEY(user_id) REFERENCES user(id))');
 
-export let addBooking = (user_id, time) => {
+export function addBooking(user_id, time) {
     return new Promise((resolve, reject) => {
-      const sql = "INSERT INTO booking (user_id, time) VALUES (?, ?);";
-      db.run(sql, [user_id, time], (err) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(null);
-      });
+        const sql = "INSERT INTO booking (user_id, time) VALUES (?, ?);";
+        db.run(sql, [user_id, time], (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(null);
+        });
     })
+
 }
