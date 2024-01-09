e3fabbb2a21d4be3735c8c8827af88a3ec8b203f
Donato Modugno <donato.modugno@gmail.com>
2024-01-09 17:09:32 +0100

    Added bookings filters

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,27 +2,11 @@
 // import { RouterLink, RouterView } from 'vue-router'
 import { HomePage } from './components'
 import Icons from './components/icons/'
-const buttons = [
-  {title:'Bookings',route:'/inlab'},
-  {title:'Stat',route:'/stat'},
-]
 </script>
 
 <template>
-  <nav>
-    <img src="./assets/weee.png" height="26" class="logo"/>
-    <span v-for="b in buttons">{{b.title}}</span>
-  </nav>
-  <main>
-    <h1>Grillo</h1>
-  </main>
-  <!-- <HomePage/>
-  <br/>
-  <Icons.IconDocumentation/>
-  <Icons.IconDocumentation/>
-  <Icons.IconDocumentation/>
-  <Icons.IconDocumentation/> -->
-  
+  <HomePage/>
+  <!-- <Icons.IconDocumentation/> -->  
 </template>
 
 <style scoped>
--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,20 +1,37 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: false
+<script>
+import Bookings from './Bookings.vue'
+export default {
+  components: {
+    Bookings
+  },
+  data() {
+    return {
+      buttons: [
+        {title:'Bookings',route:'/inlab'},
+        {title:'Stat',route:'/stat'},
+      ]
+    }
+  },
+  methods: {
+    alza() {
+      this.conta++
+    },
+    ciaone() {
+      alert('tanti saluti')
+    },
+  },
+  mounted() {
   }
-})
-function ciaone() {
-  alert('ciao claudia')
 }
 </script>
 
 <template>
-  <button @click="ciaone">ciaone</button>
-  <button>ciaone</button>
-  <button>ciaone</button>
+  <nav>
+    <img src="../assets/weee.png" height="26" class="logo"/>
+    <span v-for="b in buttons">{{b.title}}</span>
+  </nav>
+  <main>
+    <h1>Grillo</h1>
+    <Bookings/>
+  </main>
 </template>
-
-<style>
-</style>
