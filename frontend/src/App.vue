3e244db0c28608b6913501c3aeaa6cc5ffed1317
enrico <Leone25>
2025-05-16 10:55:43 +0200

    yet again more edits to the ring button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -195,13 +195,13 @@ export default {
 										<template v-if="now - ringLastPress < 5000">
 											<v-progress-circular size="48" :model-value="(now - ringLastPress) / 50" />
 											<div class="position-absolute">
-												<v-icon icon="mdi-bell-outline" />
+												<v-icon class="ring" icon="mdi-bell-outline" />
 											</div>
 										</template>
 										<template v-else-if="ringCooldown > now">
 											<v-progress-circular size="48" :model-value="(ringCooldown - now) / 50" />
 											<div class="position-absolute">
-												<v-icon icon="mdi-bell-outline" />
+												<v-icon class="ring" icon="mdi-bell-outline" />
 											</div>
 										</template>
 										<template v-else>
@@ -271,7 +271,6 @@ export default {
 					<template #activator="{ props }">
 						<v-btn
 							v-bind="props"
-							prepend-icon="mdi-bell-outline"
 							variant="text"
 							stacked
 							style="width: 128px; height: 128px"
@@ -280,6 +279,12 @@ export default {
 							@click="handleRing()"
 							:disabled="ringCooldown > now"
 						>
+							<template #prepend>
+								<v-icon
+									icon="mdi-bell-outline"
+									:class="{ ring: now - ringLastPress < 5000 || ringCooldown > now }"
+								/>
+							</template>
 							<v-progress-linear
 								v-if="now - ringLastPress < 5000"
 								:model-value="(now - ringLastPress) / 50"
@@ -333,3 +338,307 @@ export default {
 		<RouterView />
 	</v-app>
 </template>
+<style>
+.ring {
+	-webkit-animation: ring 4s 0.7s ease-in-out infinite;
+	-webkit-transform-origin: 50% 4px;
+	-moz-animation: ring 4s 0.7s ease-in-out infinite;
+	-moz-transform-origin: 50% 4px;
+	animation: ring 4s 0.7s ease-in-out infinite;
+	transform-origin: 50% 4px;
+}
+
+@-webkit-keyframes ring {
+	0% {
+		-webkit-transform: rotateZ(0);
+	}
+
+	1% {
+		-webkit-transform: rotateZ(30deg);
+	}
+
+	3% {
+		-webkit-transform: rotateZ(-28deg);
+	}
+
+	5% {
+		-webkit-transform: rotateZ(34deg);
+	}
+
+	7% {
+		-webkit-transform: rotateZ(-32deg);
+	}
+
+	9% {
+		-webkit-transform: rotateZ(30deg);
+	}
+
+	11% {
+		-webkit-transform: rotateZ(-28deg);
+	}
+
+	13% {
+		-webkit-transform: rotateZ(26deg);
+	}
+
+	15% {
+		-webkit-transform: rotateZ(-24deg);
+	}
+
+	17% {
+		-webkit-transform: rotateZ(22deg);
+	}
+
+	19% {
+		-webkit-transform: rotateZ(-20deg);
+	}
+
+	21% {
+		-webkit-transform: rotateZ(18deg);
+	}
+
+	23% {
+		-webkit-transform: rotateZ(-16deg);
+	}
+
+	25% {
+		-webkit-transform: rotateZ(14deg);
+	}
+
+	27% {
+		-webkit-transform: rotateZ(-12deg);
+	}
+
+	29% {
+		-webkit-transform: rotateZ(10deg);
+	}
+
+	31% {
+		-webkit-transform: rotateZ(-8deg);
+	}
+
+	33% {
+		-webkit-transform: rotateZ(6deg);
+	}
+
+	35% {
+		-webkit-transform: rotateZ(-4deg);
+	}
+
+	37% {
+		-webkit-transform: rotateZ(2deg);
+	}
+
+	39% {
+		-webkit-transform: rotateZ(-1deg);
+	}
+
+	41% {
+		-webkit-transform: rotateZ(1deg);
+	}
+
+	43% {
+		-webkit-transform: rotateZ(0);
+	}
+
+	100% {
+		-webkit-transform: rotateZ(0);
+	}
+}
+
+@-moz-keyframes ring {
+	0% {
+		-moz-transform: rotate(0);
+	}
+
+	1% {
+		-moz-transform: rotate(30deg);
+	}
+
+	3% {
+		-moz-transform: rotate(-28deg);
+	}
+
+	5% {
+		-moz-transform: rotate(34deg);
+	}
+
+	7% {
+		-moz-transform: rotate(-32deg);
+	}
+
+	9% {
+		-moz-transform: rotate(30deg);
+	}
+
+	11% {
+		-moz-transform: rotate(-28deg);
+	}
+
+	13% {
+		-moz-transform: rotate(26deg);
+	}
+
+	15% {
+		-moz-transform: rotate(-24deg);
+	}
+
+	17% {
+		-moz-transform: rotate(22deg);
+	}
+
+	19% {
+		-moz-transform: rotate(-20deg);
+	}
+
+	21% {
+		-moz-transform: rotate(18deg);
+	}
+
+	23% {
+		-moz-transform: rotate(-16deg);
+	}
+
+	25% {
+		-moz-transform: rotate(14deg);
+	}
+
+	27% {
+		-moz-transform: rotate(-12deg);
+	}
+
+	29% {
+		-moz-transform: rotate(10deg);
+	}
+
+	31% {
+		-moz-transform: rotate(-8deg);
+	}
+
+	33% {
+		-moz-transform: rotate(6deg);
+	}
+
+	35% {
+		-moz-transform: rotate(-4deg);
+	}
+
+	37% {
+		-moz-transform: rotate(2deg);
+	}
+
+	39% {
+		-moz-transform: rotate(-1deg);
+	}
+
+	41% {
+		-moz-transform: rotate(1deg);
+	}
+
+	43% {
+		-moz-transform: rotate(0);
+	}
+
+	100% {
+		-moz-transform: rotate(0);
+	}
+}
+
+@keyframes ring {
+	0% {
+		transform: rotate(0);
+	}
+
+	1% {
+		transform: rotate(30deg);
+	}
+
+	3% {
+		transform: rotate(-28deg);
+	}
+
+	5% {
+		transform: rotate(34deg);
+	}
+
+	7% {
+		transform: rotate(-32deg);
+	}
+
+	9% {
+		transform: rotate(30deg);
+	}
+
+	11% {
+		transform: rotate(-28deg);
+	}
+
+	13% {
+		transform: rotate(26deg);
+	}
+
+	15% {
+		transform: rotate(-24deg);
+	}
+
+	17% {
+		transform: rotate(22deg);
+	}
+
+	19% {
+		transform: rotate(-20deg);
+	}
+
+	21% {
+		transform: rotate(18deg);
+	}
+
+	23% {
+		transform: rotate(-16deg);
+	}
+
+	25% {
+		transform: rotate(14deg);
+	}
+
+	27% {
+		transform: rotate(-12deg);
+	}
+
+	29% {
+		transform: rotate(10deg);
+	}
+
+	31% {
+		transform: rotate(-8deg);
+	}
+
+	33% {
+		transform: rotate(6deg);
+	}
+
+	35% {
+		transform: rotate(-4deg);
+	}
+
+	37% {
+		transform: rotate(2deg);
+	}
+
+	39% {
+		transform: rotate(-1deg);
+	}
+
+	41% {
+		transform: rotate(1deg);
+	}
+
+	43% {
+		transform: rotate(0);
+	}
+
+	100% {
+		transform: rotate(0);
+	}
+}
+</style>
